550ac7390ac7c930274e1f04e955b13baaf3ad88
jerryjyguan <jerryjyguan@tencent.com>
2023-05-12 11:41:39 +0800

    更新Puppeteer的接口

--- adapter/adapter.js
+++ adapter/adapter.js
@@ -1,8 +1,12 @@
+import fs from 'node:fs'
+import yaml from 'yaml'
 import PuppeteerRenderer from "../../../renderers/puppeteer/lib/puppeteer.js";
 import puppeteer from "../../../lib/puppeteer/puppeteer.js";
 
-function getRendererCfg() {
-  // 临时处理 等待新接口
+let yunzaiEdition = null;
+let rendererCfg = null;
+
+function getTempDefaultRendererCfg() {
   return {
     headless: true,
     args: [
@@ -14,9 +18,50 @@ function getRendererCfg() {
   };
 }
 
-export function getPuppeteer() {
-  if (!!puppeteer.browserInit) {
-    return puppeteer;
+function getMiaoYunzaiRendererCfg() {
+  if (!rendererCfg) {
+    let configFile = './renderers/puppeteer/config.yaml';
+    if (fs.existsSync(configFile)) {
+      try {
+        rendererCfg = yaml.parse(fs.readFileSync(configFile, 'utf8'));
+      } catch (e) {
+        rendererCfg = getTempDefaultRendererCfg();
+      }
+    } else {
+      rendererCfg = getTempDefaultRendererCfg();
+    }
   }
-  return new PuppeteerRenderer(getRendererCfg());
+  return rendererCfg;
+}
+
+function getPm2Cfg() {
+  const configFile = './config/pm2/pm2.json';
+  if (fs.existsSync(configFile)) {
+    let pmcfg = fs.readFileSync(configFile);
+    pmcfg = JSON.parse(pmcfg);
+    return pmcfg;
+  }
+  return null;
+}
+
+function getStaticEditionTag() {
+  const pmcfg = getPm2Cfg();
+  if (pmcfg) {
+    return pmcfg.apps[0].name;
+  }
+  return 'Yunzai-Bot';
+}
+
+function getYunzaiEdition() {
+  if (!yunzaiEdition) {
+    yunzaiEdition = getStaticEditionTag();
+  }
+  return yunzaiEdition;
+}
+
+export function getPuppeteer() {
+  if (getYunzaiEdition() == 'Miao-Yunzai') {
+    return new PuppeteerRenderer(getMiaoYunzaiRendererCfg());
+  }
+  return puppeteer;
 }
